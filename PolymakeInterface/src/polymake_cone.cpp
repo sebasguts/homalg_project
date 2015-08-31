ac2b7c918ea4a9cf950825f784b7211ad209fee2
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2015-08-31 18:31:30 +0200

    Stupid

--- PolymakeInterface/src/polymake_cone.cpp
+++ PolymakeInterface/src/polymake_cone.cpp
@@ -336,15 +336,13 @@ Obj REAL_GENERATING_RAYS_OF_CONE( Polymake_Data* data, Obj cone){
     lcm = 1;
     
     for( int j = 0; j < matr_cols_signed; j++ ){
-      pm::lcm( lcm, denom_array[ j ] );
+      lcm = pm::lcm( lcm, denom_array[ j ] );
       cerr << denom_array[ j ] << ", " << lcm << ", " << pm::lcm( lcm, denom_array[ j ] ) << std::endl;
     }
     
-    pm::Rational lcm_rat( lcm );
-    
     for( int j = 0; j < matr_cols_signed; j++ ){
-      convert_array[ j ] = convert_array[ j ] * lcm_rat;
-      cerr << "Eintrag: " << lcm_rat << ", " << convert_array[ j ] << std::endl;
+      convert_array[ j ] = convert_array[ j ] * lcm;
+      cerr << "Eintrag: " << lcm << ", " << convert_array[ j ] << std::endl;
     }
     
     for(int j = 0;j<matr.cols();j++){
